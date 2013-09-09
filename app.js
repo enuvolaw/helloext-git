d6b8b889500acdc7168f7608878f4dd636ba8861
enuvolaw <w.villa@enuvola.com>
2013-09-09 09:54:32 +0200

    some deletes and modify app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 Ext.require('Ext.container.Viewport');
 
 Ext.application({
-    name: 'HelloExt',
+    name: 'HelloExtGit',
     launch: function() {
         Ext.create('Ext.container.Viewport', {
             layout: 'fit',
             items: [
                 {
                     title: 'Hello Ext Git',
-                    html : 'Hello! Welcome to Ext JS.'
+                    html : 'Hello! Welcome to Ext JS on GitHub.'
                 }
             ]
         });
